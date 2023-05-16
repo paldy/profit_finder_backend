73327b650df8f64243f8bd943962b5de8cac4882
paldy <58873376+paldy@users.noreply.github.com>
2023-05-16 11:39:00 +0200

    delete To file, add to DB

--- request_sbr.py
+++ request_sbr.py
@@ -17,8 +17,6 @@ def parsing_data(currency, xml):
 # https://www.cbr.ru/development/SXML/
 def sbr_request(input_data):
     r = requests.post('https://www.cbr.ru/scripts/XML_daily.asp')
-    # with open(os.path.dirname(__file__) + 'sbr.xml', 'w') as j:
-    #     print(r.text, file=j)  # записываем строку в файл
 
     result = parsing_data(input_data.asset, r.text)
 
